be87f7cb0bcff0adedd32c4feae353dac1328e13
Jon McLean <37740@cgs.act.edu.au>
2015-07-21 12:59:03 +1000

    New debug line

--- Test Project/ViewController.swift
+++ Test Project/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello Git")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
