47462177c72f535da67ab229aad67861790cebe1
Jon McLean <37740@cgs.act.edu.au>
2015-07-30 15:03:33 +1000

    New View Controller - SecondViewController.swift

---
+++ Test Project/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  Test Project
+//
+//  Created by Jon on 30/07/2015.
+//  Copyright (c) 2015 Actro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func coolFeature() {
+        println("This is a cool feature")
+        // This is a really cool comment.
+    }
+    
+}
